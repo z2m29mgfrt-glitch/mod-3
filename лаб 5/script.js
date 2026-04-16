423a924b5f30622125d51a8fc63b6da198500f1a
z2m29mgfrt-glitch <z2m29mgfrt@privaterelay.appleid.com>
2026-04-16 23:55:25 +0400

    lab-5

--- лаб 5/script.js
+++ лаб 5/script.js
@@ -1,14 +1,20 @@
-
 $('#loadBtn').on('click', function() {
+  const result = $('#result');
+  result.text('Загружаем данные...').addClass('loading').removeClass('error');
+
   fetch('https://jsonplaceholder.typicode.com/users/1')
     .then(function(response) {
+      if (!response.ok) {
+        throw new Error('HTTP error! Status: ' + response.status);
+      }
       return response.json();
     })
     .then(function(data) {
-      let text = 'Имя: ' + data.name + ' | Email: ' + data.email;
-      $('#result').text(text);
+      result.text('Имя: ' + data.name + ' | Email: ' + data.email)
+           .removeClass('loading');
     })
     .catch(function(error) {
-      $('#result').text('Ошибка загрузки! Проверьте интернет.');
+      console.error('Ошибка:', error);
+      result.text('Ошибка: ' + error.message).addClass('error').removeClass('loading');
     });
 });
